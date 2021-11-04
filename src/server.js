c4e13337680e227e7352fb9d342a82cbfefa5e59
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-11-04 11:52:42 -0300

    .

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const server = express()
+const routes = require("./routes")
+
+// habilitar arquivos estáticos
+server.use(express.static("public"))
+
+// routes
+server.use(routes)
+
+server.listen(3000, () => console.log('rodando'))
