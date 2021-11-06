fd37a372094dbbf7e22c18c172f293ab33d05ed0
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-11-06 18:13:26 -0300

    .

--- src/routes.js
+++ src/routes.js
@@ -5,16 +5,48 @@ const basePath = __dirname + '/views/' // atalho para facilitar os diretórios d
 // alterar os dados do perfil
 const profile = {
   name: 'Huann',
-  avatar: 'https://avatars.githubusercontent.com/u/67878933?v=4',
+  avatar: 'https://github.com/huannvictor.png',
   'monthly-budget': 3000,
   'days-per-week': 5,
   'hours-per-day': 5,
   'vacation-per-year': 4
 }
 
+const jobs = [
+  {
+    id: 1,
+    name: 'Pizzaria Guloso',
+    'daily-hours': 2,
+    'total-hours': 60,
+    created_at: Date.now()
+  },
+  {
+    id: 2,
+    name: 'One Two Project',
+    'daily-hours': 3,
+    'total-hours': 47,
+    created_at: Date.now()
+  }
+]
+
 // req, res
-routes.get('/', (req, res) => res.render(basePath + 'index'))
+routes.get('/', (req, res) =>
+  res.render(basePath + 'index', { jobs })
+)
 routes.get('/job', (req, res) => res.render(basePath + 'job'))
+routes.post('/job', (req, res) => {
+  // req,body { name: 'HUANN', 'daily-hours': '2', 'total-hours': '2' }
+  const lastId = jobs[jobs.length - 1]?.id || 1
+
+  jobs.push({
+    id: lastId + 1,
+    name: req.body.name,
+    'daily-hours': req.body['daily-hours'],
+    'total-hours': req.body['total-hours'],
+    created_at: Date.now() // atribuindo data de hoje
+  })
+  return res.redirect('/')
+})
 routes.get('/job/edit', (req, res) => res.render(basePath + 'job-edit'))
 routes.get('/profile', (req, res) =>
   res.render(basePath + 'profile', { profile })
