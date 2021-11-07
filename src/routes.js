6f5a4f91d96583bb0d4d1283f8f95a3df70f98ba
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-11-07 09:35:00 -0300

    finishing aula 2

--- src/routes.js
+++ src/routes.js
@@ -86,7 +86,7 @@ const Job = {
     },
     save(req, res) {
       // req,body { name: 'HUANN', 'daily-hours': '2', 'total-hours': '2' }
-      const lastId = Job.data[Job.data.length - 1]?.id || 1
+      const lastId = Job.data[Job.data.length - 1]?.id || 0
 
       Job.data.push({
         id: lastId + 1,
@@ -135,8 +135,16 @@ const Job = {
       })
 
       res.redirect('/job/' + jobId)
+    },
+    delete(req, res) {
+      const jobId = req.params.id
+
+      Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId))
+
+      return res.redirect('/')
     }
   },
+
   services: {
     remainingDays(job) {
       // cálculo do tempo restante
@@ -161,6 +169,7 @@ routes.get('/job', Job.controllers.create)
 routes.post('/job', Job.controllers.save)
 routes.get('/job/:id', Job.controllers.show)
 routes.post('/job/:id', Job.controllers.update)
+routes.post('/job/delete/:id', Job.controllers.delete)
 routes.get('/profile', Profile.controllers.index)
 routes.post('/profile', Profile.controllers.update)
 
