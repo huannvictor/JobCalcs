883ee541f65e84b04782137cb351e02db061e6fb
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-11-07 15:23:02 -0300

    dashboard finished

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -8,10 +8,31 @@ module.exports = {
     const jobs = Job.get()
     const profile = Profile.get()
 
+    const statusCount = {
+      progress: 0,
+      done: 0,
+      total: jobs.length
+    }
+
+    // total de horas por dia de cada job em PROGRESS
+    let jobTotalHours = 0
+
     const updatedJobs = jobs.map(job => {
       // ajustes no jobs
       const remaining = JobUtils.remainingDays(job)
       const status = remaining <= 0 ? 'done' : 'progress'
+
+      // a const status é passada como referência, dento dos [] -> se é done || progress
+      // informa dendo dos [] a referência (done || progress)
+      // o cógigo fica como se fosse status.done += 1 || status.progress += 1
+      // obs.: quando coloca dentro de [] uma referência, é passado como se fosse uma propriedade do objeto (object.value)
+      statusCount[status] += 1
+
+      // total de horas por dia de cada job em PROGRESS
+      jobTotalHours =
+        status == 'progress'
+          ? jobTotalHours + Number(job['daily-hours'])
+          : jobTotalHours
 
       return {
         ...job,
@@ -21,6 +42,16 @@ module.exports = {
       }
     })
 
-    return res.render('index', { jobs: updatedJobs })
+    // quantidade de hrs que quero trabalhar (PROFILE)
+    // MENOS
+    // a quantidade de hrs de cada job in PROGRESS
+    const freeHours = profile['hours-per-day'] - jobTotalHours
+
+    return res.render('index', {
+      jobs: updatedJobs,
+      profile,
+      statusCount,
+      freeHours
+    })
   }
 }
